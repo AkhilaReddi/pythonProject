d17e07692b235bda2f9e87d4043ad8f57e71cd68
akhila <ravulaakhilareddy@gmail.com>
2021-11-23 22:13:57 +0530

    first commit

---
+++ pythonProject/main.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+s = Service("C://Users//AkhilaReddy//Desktop//Akhila documents//Automation//chromedriver.exe")
+driver = webdriver.Chrome(service=s)
+driver.get("https://www.airdriven.com/")
+driver.maximize_window()
+driver.close()
